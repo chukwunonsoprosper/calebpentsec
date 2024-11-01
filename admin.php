3a7d7b2fce9f0153b2123db5ef14f5af597a9b03
= <muthuannamalai@example.com>
2024-11-01 17:55:33 +0100

    main

--- admin.php
+++ admin.php
@@ -7,7 +7,7 @@ include './connection/database.php'
 <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>C4L3B21 BLOG</title>
+    <title>C4L3B21 BLOG (admin)</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
     <script src='./js/main.js'></script>
@@ -61,7 +61,7 @@ include './connection/database.php'
                     function validate($data)
                     {
                         $data = trim($data);
-                        $dagit ta = htmlspecialchars($data);
+                        $data = htmlspecialchars($data);
                         $data = stripslashes($data);
                         return $data;
                     }
@@ -101,7 +101,7 @@ include './connection/database.php'
 <head>
     <meta charset='UTF-8' />
     <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-    <title>Document</title>
+    <title>{$blogTitle}</title>
     <link rel='stylesheet' href='../style/style.css' />
     <link rel='stylesheet' href='../style/modified.css' />
         <link rel='stylesheet' href='../style/mobile.css'>
